03ea65a549781ebf0063116a327701e44519f2eb
Yakymenko Vladyslav <vladyslav.yakymenko.ihorovych@gmail.com>
2022-07-04 20:16:11 +0300

    feat! (back-end, shared/api): shared - internalFeatureProvider impl, shared infrastructure extension method add / api - addSharedInfrustructure use, reference to shared add

---
+++ src/DailyManager/DM.Shared.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using DM.Shared.Infrastructure.Controllers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DM.Shared.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSharedInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            services
+                .AddControllers()
+                .ConfigureApplicationPartManager(manager
+                    => manager.FeatureProviders.Add(new InternalControllerFeatureProvider()));
+
+            return services;
+        }
+    }
+}
