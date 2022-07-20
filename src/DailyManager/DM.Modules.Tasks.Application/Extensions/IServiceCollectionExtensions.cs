621f4d39ccddf7a4aac1e33dbb203928b5a3245b
Yakymenko Vladyslav <vladyslav.yakymenko.ihorovych@gmail.com>
2022-07-20 20:20:07 +0300

    init/feat! (back-end, tasks): application - project add / application, extensions - iServiceCollectionExtensions to add mapping, application layer impl

---
+++ src/DailyManager/DM.Modules.Tasks.Application/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using DM.Shared.Application.Mapping;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace DM.Modules.Tasks.Application.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTasksApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMappings(configuration);
+
+            return services;
+        }
+
+        #region Private methods
+
+        public static IServiceCollection AddMappings(this IServiceCollection services, IConfiguration config)
+        {
+            new MapperRegistrator().FromAssembly(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+
+        #endregion
+    }
+}
