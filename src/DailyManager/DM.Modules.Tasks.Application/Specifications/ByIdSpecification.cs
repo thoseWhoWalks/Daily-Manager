0d021da7f3e34a6d2dc6c39daf3a8271e454d88d
Yakymenko Vladyslav <vladyslav.yakymenko.ihorovych@gmail.com>
2022-07-27 23:05:30 +0300

    feat!/dep (back-end, tasks): application - byIdSpecification impl, mapster dep add

---
+++ src/DailyManager/DM.Modules.Tasks.Application/Specifications/ByIdSpecification.cs
@@ -0,0 +1,16 @@
+﻿using DM.Shared.Core.Aggregates;
+using DM.Shared.Core.Entities;
+using EntityFrameworkCore.CommonTools;
+
+namespace DM.Modules.Tasks.Application.Specifications
+{
+    internal class ByIdSpecification<TAggregate> 
+        : Specification<TAggregate>
+        where TAggregate : class, IEntity, IAggregateRoot
+    {
+        public ByIdSpecification(Guid id) : base(ag => ag.Id == id)
+        {
+
+        }
+    }
+}
