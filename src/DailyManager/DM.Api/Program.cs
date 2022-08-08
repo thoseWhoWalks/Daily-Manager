fdfe5f587d46f8318f1c54b6c014febfddc706ab
Yakymenko Vladyslav <vladyslav.yakymenko.ihorovych@gmail.com>
2022-08-08 19:22:21 +0300

    feat! (back-end, shared/api): shared, infrastructure - useDbConsistencyPolicy applicationBuilderExtension method impl / api - shared infrastructure use

--- src/DailyManager/DM.Api/Program.cs
+++ src/DailyManager/DM.Api/Program.cs
@@ -22,6 +22,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSharedInfrastructure();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
---
+++ src/DailyManager/DM.Shared.Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DM.Shared.Infrastructure.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        #region Constants
+
+        private const string DmAssemblyPrefix = "DM.Modules.";
+
+        #endregion
+
+        public static IApplicationBuilder UseSharedInfrastructure(this IApplicationBuilder builder)
+        {
+            builder
+                .UseDbConsistencyPolicy();
+
+            return builder;
+        }
+
+        #region Private methods
+
+        private static IApplicationBuilder UseDbConsistencyPolicy(this IApplicationBuilder builder)
+        {
+            var dbContextTypes = AppDomain.CurrentDomain
+                .GetAssemblies().Where(a => !string.IsNullOrEmpty(a.FullName) && a.FullName.Contains(DmAssemblyPrefix))
+                .SelectMany(@as => @as.GetTypes().Where(t => t.IsSubclassOf(typeof(DbContext))));
+
+            using var scope = builder.ApplicationServices.CreateScope();
+
+            foreach (var type in dbContextTypes)
+            {
+                var context = scope.ServiceProvider.GetRequiredService(type) as DbContext;
+
+                context?.Database.EnsureCreated();
+                context?.Database.Migrate();
+            }
+
+            return builder;
+        }
+
+        #endregion
+    }
+}
