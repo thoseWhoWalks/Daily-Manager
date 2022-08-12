76ecadf7c3c9c4c467679b4f45bb3927ed3f4f33
Yakymenko Vladyslav <vladyslav.yakymenko.ihorovych@gmail.com>
2022-08-12 17:17:36 +0300

    feat! (back-end, tasks): application, mapping - taskMapsterConfig, taskListMapsterConfig add

---
+++ src/DailyManager/DM.Modules.Tasks.Application/Mapping/TaskLists/TaskListMapsterConfig.cs
@@ -0,0 +1,42 @@
+﻿using DM.Modules.Tasks.Core.Aggregates;
+using DM.Modules.Tasks.Shared.Models.TaskLists;
+using DM.Shared.Application.Mapping;
+using Mapster;
+
+namespace DM.Modules.Tasks.Application.Mapping.TaskLists
+{
+    internal class TaskListMapsterConfig : IMapperConfig
+    {
+        public void Create()
+        {
+            CreateTaskListMap();
+        }
+
+        #region Private methods
+
+        private void CreateTaskListMap()
+        {
+            TypeAdapterConfig<TaskList, TaskListModel>
+                .NewConfig()
+                .Map(dest => dest.Title,
+                     model => model.Title)
+                .Map(dest => dest.Description,
+                     model => model.Description)
+                .Map(dest => dest.IsDeleted,
+                     model => model.IsDeleted)
+                .Map(dest => dest.CreatedAt,
+                     model => model.CreatedAt)
+                .Map(dest => dest.DeletedAt,
+                     model => model.DeletedAt);
+
+            TypeAdapterConfig<TaskList, TaskListListItemModel>
+                .NewConfig()
+                .Map(dest => dest.Title,
+                     model => model.Title)
+                .Map(dest => dest.OpenedTaskCount,
+                     model => model.OpenedTaskCount);
+        }
+
+        #endregion
+    }
+}
---
+++ src/DailyManager/DM.Modules.Tasks.Application/Mapping/Tasks/TaskMapsterConfig.cs
@@ -0,0 +1,48 @@
+﻿using DM.Modules.Tasks.Shared.Models.Tasks;
+using DM.Shared.Application.Mapping;
+using Mapster;
+using Task = DM.Modules.Tasks.Core.Aggregates.Task;
+
+namespace DM.Modules.Tasks.Application.Mapping.Tasks
+{
+    internal class TaskMapsterConfig : IMapperConfig
+    {
+        public void Create()
+        {
+            CreateTaskMap();
+        }
+
+        #region Private methods
+
+        private void CreateTaskMap()
+        {
+            TypeAdapterConfig<Task, TaskModel>
+                .NewConfig()
+                .Map(dest => dest.Title,
+                     model => model.Title)
+                .Map(dest => dest.Description,
+                     model => model.Description)
+                .Map(dest => dest.State,
+                     model => model.State)
+                .Map(dest => dest.IsDeleted,
+                     model => model.IsDeleted)
+                .Map(dest => dest.CreatedAt,
+                     model => model.CreatedAt)
+                .Map(dest => dest.DeletedAt,
+                     model => model.DeletedAt)
+                .Map(dest => dest.ExecuteAt,
+                     model => model.ExecuteAt)
+                .Map(dest => dest.ExecutedAt,
+                     model => model.ExecutedAt);
+
+            TypeAdapterConfig<Task, TaskListItemModel>
+                .NewConfig()
+                .Map(dest => dest.Title,
+                     model => model.Title)
+                .Map(dest => dest.State,
+                     model => model.State);
+        }
+
+        #endregion
+    }
+}
