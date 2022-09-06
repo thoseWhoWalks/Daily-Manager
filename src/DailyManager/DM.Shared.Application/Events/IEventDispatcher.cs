1c765ca3fbb0b3bd1c46cebbd4d32516bec7148a
Yakymenko Vladyslav <vladyslav.yakymenko.ihorovych@gmail.com>
2022-09-06 19:09:53 +0300

    upd/feat (back-end, shared): application, events - iEventDispatcher contract to accept events array / infrastructure, events - eventDispatcher in outBox context impl

--- src/DailyManager/DM.Shared.Application/Events/IEventDispatcher.cs
+++ src/DailyManager/DM.Shared.Application/Events/IEventDispatcher.cs
@@ -4,9 +4,9 @@ namespace DM.Shared.Application.Events
 {
     public interface IEventDispatcher
     {
-        void Publish<TEvent>(TEvent @event)
+        void Publish<TEvent>(TEvent[] @event)
             where TEvent : class, IEvent;
-        Task PublishAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default)
+        Task PublishAsync<TEvent>(TEvent[] @event, CancellationToken cancellationToken = default)
             where TEvent : class, IEvent;
     }
 }
---
+++ src/DailyManager/DM.Shared.Infrastructure/Events/EventDispatcher.cs
@@ -0,0 +1,32 @@
+﻿using DM.Shared.Application.Events;
+using DM.Shared.OutBox.Context;
+using DM.Shared.OutBox.Entities;
+
+namespace DM.Shared.Infrastructure.Events
+{
+    public abstract class EventDispatcher : IEventDispatcher
+    {
+        #region Dependecies
+        private readonly OutBoxDbContext _dbContext;
+        #endregion
+
+        public EventDispatcher(OutBoxDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        void IEventDispatcher.Publish<TEvent>(TEvent[] @event)
+        {
+            var messages = @event.Select(e => new OutBoxMessage(e));
+
+            _dbContext.Add(messages);
+        }
+
+        async Task IEventDispatcher.PublishAsync<TEvent>(TEvent[] @event, CancellationToken cancellationToken)
+        {
+            var messages = @event.Select(e => new OutBoxMessage(e));
+
+            await _dbContext.AddAsync(messages, cancellationToken);
+        }
+    }
+}
