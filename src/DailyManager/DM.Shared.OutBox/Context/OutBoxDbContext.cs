92b2b025e6748e506add869e6eeef20c6ae5e749
Yakymenko Vladyslav <vladyslav.yakymenko.ihorovych@gmail.com>
2022-09-06 19:07:30 +0300

    feat! (back-end, out-box): project init, context, message entity add

---
+++ src/DailyManager/DM.Shared.OutBox/Context/OutBoxDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DM.Shared.OutBox.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DM.Shared.OutBox.Context
+{
+    public abstract class OutBoxDbContext : DbContext
+    {
+        public DbSet<OutBoxMessage> OutBoxMessages { get; set; } = null!;
+
+        public OutBoxDbContext(DbContextOptions<OutBoxDbContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            => modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+}
---
+++ src/DailyManager/DM.Shared.OutBox/Entities/OutBoxMessage.cs
@@ -0,0 +1,24 @@
+﻿using DM.Shared.Core.Entities;
+using DM.Shared.Core.Events;
+using Newtonsoft.Json.Linq;
+
+namespace DM.Shared.OutBox.Entities
+{
+    public class OutBoxMessage : IEntity
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+
+        public string Message { get; }
+        public string Type { get; }
+
+        public DateTime CreatedAt { get; } = DateTime.UtcNow;
+        public DateTime? ExecutedAt { get; set; }
+
+        public OutBoxMessage(IEvent @event)
+        {
+            Message = JObject.FromObject(@event).ToString();
+            Type = @event.GetType().FullName
+                ?? throw new ArgumentNullException();
+        }
+    }
+}
