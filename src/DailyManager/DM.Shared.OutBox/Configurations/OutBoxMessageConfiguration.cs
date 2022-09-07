d1a3110bcdca820a3811028ee97b6e80f3624e71
Yakymenko Vladyslav <vladyslav.yakymenko.ihorovych@gmail.com>
2022-09-07 17:20:09 +0300

    feat (back-end, out-box): out-box-message configuration add

---
+++ src/DailyManager/DM.Shared.OutBox/Configurations/OutBoxMessageConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using DM.Shared.OutBox.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DM.Shared.OutBox.Configurations
+{
+    internal class OutBoxMessageConfiguration : IEntityTypeConfiguration<OutBoxMessage>
+    {
+        #region Contants
+        private const int TypeMaxLength = 256;
+        private const int MessageMaxLength = 512;
+
+        protected const string DateTimeTypeName = "datetime2";
+        #endregion
+
+        public void Configure(EntityTypeBuilder<OutBoxMessage> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(u => u.Type)
+                .HasMaxLength(TypeMaxLength)
+                .IsRequired();
+            builder.Property(u => u.Message)
+                .HasMaxLength(MessageMaxLength)
+                .IsRequired();
+
+            builder
+                .Property(obm => obm.CreatedAt)
+                .HasColumnType(DateTimeTypeName);
+            builder
+                .Property(obm => obm.ExecutedAt)
+                .HasColumnType(DateTimeTypeName);
+        }
+    }
+}
