f09cb51f81e6151f563b305ed8050cca571c01ce
Yakymenko Vladyslav <vladyslav.yakymenko.ihorovych@gmail.com>
2022-09-12 17:20:33 +0300

    feat! (back-end, users): shared - project add / userCreated, userDeleted, userUpdated events add

---
+++ src/DailyManager/DM.Modules.Users.Shared/Events/UserCreated.cs
@@ -0,0 +1,6 @@
+﻿using DM.Shared.Core.Events;
+
+namespace DM.Modules.Users.Shared.Events
+{
+    public record UserCreated(Guid UserId, string Login) : IEvent;
+}
---
+++ src/DailyManager/DM.Modules.Users.Shared/Events/UserDeleted.cs
@@ -0,0 +1,6 @@
+﻿using DM.Shared.Core.Events;
+
+namespace DM.Modules.Users.Shared.Events
+{
+    public record UserDeleted(Guid UserId, DateTime? DeletedAt) : IEvent;
+}
