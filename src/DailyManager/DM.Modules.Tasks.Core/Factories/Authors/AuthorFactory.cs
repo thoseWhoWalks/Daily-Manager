df3b94e9558f49eca3b0e464860bb403cc7fad68
Yakymenko Vladyslav <vladyslav.yakymenko.ihorovych@gmail.com>
2022-09-13 17:18:07 +0300

    feat (back-end, tasks): authors - factory impl

---
+++ src/DailyManager/DM.Modules.Tasks.Core/Factories/Authors/AuthorFactory.cs
@@ -0,0 +1,10 @@
+﻿using DM.Modules.Tasks.Core.Aggregates;
+
+namespace DM.Modules.Tasks.Core.Factories.Authors
+{
+    public class AuthorFactory : IAuthorFactory
+    {
+        public Author Create(Guid id, string login)
+            => new(id, login);
+    }
+}
---
+++ src/DailyManager/DM.Modules.Tasks.Core/Factories/Authors/IAuthorFactory.cs
@@ -0,0 +1,9 @@
+﻿using DM.Modules.Tasks.Core.Aggregates;
+
+namespace DM.Modules.Tasks.Core.Factories.Authors
+{
+    public interface IAuthorFactory  
+    {
+        Author Create(Guid id, string login);
+    }
+}
